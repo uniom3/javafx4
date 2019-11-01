a213c634140afa875888648d1fb9c6310aae08d2
uniom3 <deivid.mendonca@hotmail.com>
2019-10-31 21:12:44 -0300

    calcular com JavaFX

---
+++ src/gui/ViewController.java
@@ -0,0 +1,36 @@
+package gui;
+
+
+import java.util.Locale;
+
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class ViewController {
+	@FXML
+	private TextField txtNumber1;
+	@FXML
+	private TextField txtNumber2;
+	
+	@FXML
+	private Label labelResult;	
+	
+	@FXML
+	private Button btsum;
+	
+	@FXML
+	public void onBtSumAction() {
+		Locale.setDefault(Locale.US);
+		double number1 = Double.parseDouble(txtNumber1.getText());
+		double numer2 = Double.parseDouble(txtNumber2.getText());
+		double sum = number1 + numer2;
+		labelResult.setText(String.format("%.2f", sum));
+	
+	}
+	
+}
